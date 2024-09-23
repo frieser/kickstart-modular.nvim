1c1140780586da17c38699dede10970d1e0eb9bf
Hector <frieserpaldi@gmail.com>
2024-09-23 18:01:34 +0200

    add terminal plugin, remove comments and some personalization

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        shell = 'zsh',
+      }
+
+      vim.api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+    end,
+  },
+}
